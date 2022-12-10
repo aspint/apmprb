1c860039697554b57a150faa3c99d31640a597a0
arthur.soares <arthur.soares@fornecedores.sicoob.com.br>
2022-12-10 15:24:07 -0300

    correções do aside bar e herder bar

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -93,12 +93,12 @@
                 <!-- Comment -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-check-circle font-18"></i>
+                  <!--  <a class="nav-link dropdown-toggle waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-check-circle font-18"></i>
                         <div class="notify">
                             <span class="heartbit"></span>
                             <span class="point"></span>
                         </div>
-                    </a>
+                    </a>-->
                     <div class="dropdown-menu dropdown-menu-left mailbox animated bounceInDown">
                         <span class="with-arrow"><span class="bg-primary"></span></span>
                         <ul class="list-style-none">
@@ -146,10 +146,10 @@
                 <!-- mega menu -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown mega-dropdown">
-                    <a class="nav-link dropdown-toggle waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <!--<a class="nav-link dropdown-toggle waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="d-none d-md-block">Mega <i class="icon-options-vertical"></i></span>
                         <span class="d-block d-md-none"><i class="mdi mdi-dialpad font-24"></i></span>
-                    </a>
+                    </a>-->
                     <div class="dropdown-menu animated bounceInDown">
                         <div class="mega-dropdown-menu row">
                             <div class="col-lg-3 col-xlg-2 mb-4">
@@ -279,16 +279,10 @@
                             <div class="ml-2">
                                 <h4 class="mb-0">Steave Jobs</h4>
                                 <p class=" mb-0 text-muted">varun@gmail.com</p>
-                                <a href="javascript:void(0)" class="btn btn-sm btn-danger text-white mt-2 btn-rounded">View Profile</a>
+                                <!--<a href="javascript:void(0)" class="btn btn-sm btn-danger text-white mt-2 btn-rounded">View Profile</a>-->
                             </div>
                         </div>
-                        <a class="dropdown-item" href="javascript:void(0)"><i class="ti-user mr-1 ml-1"></i> My Profile</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i class="ti-wallet mr-1 ml-1"></i> My Balance</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i class="ti-email mr-1 ml-1"></i> Inbox</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="javascript:void(0)"><i class="ti-settings mr-1 ml-1"></i> Account Setting</a>
-                        <div class="dropdown-divider"></div>
-                        <a href="{{route('logout')}}" class="dropdown-item" href="javascript:void(0)"><i class="fa fa-power-off mr-1 ml-1"></i> Logout</a>
+                        @include('layout.dropdown');
                     </div>
                 </li>
                 <!-- ============================================================== -->
